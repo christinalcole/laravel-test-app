ade36121db7edfa6e60dd7b22d543d6047962988
Christina L. Cole <christinalcole@gmail.com>
2017-09-20 22:33:39 -0400

    change inline color to read from array

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<?php $colors = array('bg-blue','bg-orange','bg-green', 'bg-dkblue', 'bg-dkgray', 'bg-ltblue', 'bg-tan','bg-dkgray','bg-dkgray'); ?>
+<?php $colors = array('green','orange','pink', 'gray'); ?>
 
 <aside aria-label="Left Sidebar" class="hidden-xs left-sidebar">
 <h1>{{$person->name}}</h1>
@@ -10,7 +10,7 @@
 
       @if(!empty($person->Link1))
 
-        <div style="background-color: green;"><a href="#news" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[0]}};"><a href="#news" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">News</span></span></div>
           <!-- end inner --></a></div>
         <!-- end navbox -->
@@ -19,7 +19,7 @@
 
       @if(!empty($person->Link2))
 
-        <div style="background-color: orange;"><a href="#events" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[1]}};"><a href="#events" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Events</span></span></div>
           <!-- end inner --></a></div>
         <!-- end navbox -->
@@ -27,7 +27,7 @@
 
       @if(!empty($person->Link3))
 
-        <div style="background-color: pink;"><a href="#publications" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[2]}};"><a href="#publications" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Publications</span></span></div>
           <!-- end inner --></a></div>
         <!-- end navbox -->
@@ -36,7 +36,7 @@
 
       @if(!empty($person->Link4))
 
-        <div style="background-color: gray;"><a href="#extras" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[3]}};"><a href="#extras" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Extras</span></span></div>
           <!-- end inner --></a></div>
         <!-- end navbox -->
