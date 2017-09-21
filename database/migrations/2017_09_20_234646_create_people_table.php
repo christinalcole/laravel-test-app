f4c569df1ae535d2752c034c4bb3ad5e026dfe9f
Christina L. Cole <christinalcole@gmail.com>
2017-09-20 21:03:05 -0400

    add Person model

---
+++ database/migrations/2017_09_20_234646_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('Link1');
+            $table->text('Link2');
+            $table->text('Link3');
+            $table->text('Link4');
+            $table->text('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
