a806244b80e22c2418caeffbacf15447669f4a0a
Christina L. Cole <christinalcole@gmail.com>
2017-09-20 23:12:27 -0400

    update array read to autoincrement index

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -1,44 +1,51 @@
 
 
 
-<?php $colors = array('green','orange','pink', 'gray'); ?>
+<?php $colors = array('green','orange','pink', 'gray');
+  $i = 0; ?>
+
 
 <aside aria-label="Left Sidebar" class="hidden-xs left-sidebar">
 <h1>{{$person->name}}</h1>
 
-      <?php $news_color = false;  ?>
+      <!-- <?php $news_color = false;  ?> -->
 
       @if(!empty($person->Link1))
 
-        <div style="background-color: {{$colors[0]}};"><a href="#news" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[$i]}};"><a href="#news" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">News</span></span></div>
           <!-- end inner --></a></div>
+          <? ++$i ?>
         <!-- end navbox -->
-        <?php $news_color = true;  ?>
+
+        <!-- <?php $news_color = true;  ?> -->
       @endif
 
       @if(!empty($person->Link2))
 
-        <div style="background-color: {{$colors[1]}};"><a href="#events" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[$i]}};"><a href="#events" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Events</span></span></div>
           <!-- end inner --></a></div>
+          <? ++$i ?>
         <!-- end navbox -->
       @endif
 
       @if(!empty($person->Link3))
 
-        <div style="background-color: {{$colors[2]}};"><a href="#publications" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[$i]}};"><a href="#publications" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Publications</span></span></div>
           <!-- end inner --></a></div>
+          <? ++$i ?>
         <!-- end navbox -->
 
       @endif
 
       @if(!empty($person->Link4))
 
-        <div style="background-color: {{$colors[3]}};"><a href="#extras" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[$i]}};"><a href="#extras" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Extras</span></span></div>
           <!-- end inner --></a></div>
+          <? ++$i ?>
         <!-- end navbox -->
 
       @endif
