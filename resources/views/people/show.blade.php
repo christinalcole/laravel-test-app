cf40d60052c2c0282f90c20cc6b50f97a7580502
Christina L. Cole <christinalcole@gmail.com>
2017-09-20 23:36:55 -0400

    update array read to cycle through indices then repeat

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<?php $colors = array('green','orange','pink', 'gray');
+<?php $colors = array('green','orange','pink');
   $i = 0; ?>
 
 
@@ -12,7 +12,9 @@
 
       @if(!empty($person->Link1))
 
-        <div style="background-color: {{$colors[$i]}};"><a href="#news" title="" data-toggle="tab">
+
+
+        <div style="background-color: {{$colors[($i%count($colors))]}};"><a href="#news" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">News</span></span></div>
           <!-- end inner --></a></div>
           <? ++$i ?>
@@ -23,7 +25,7 @@
 
       @if(!empty($person->Link2))
 
-        <div style="background-color: {{$colors[$i]}};"><a href="#events" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[($i%count($colors))]}};"><a href="#events" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Events</span></span></div>
           <!-- end inner --></a></div>
           <? ++$i ?>
@@ -32,7 +34,7 @@
 
       @if(!empty($person->Link3))
 
-        <div style="background-color: {{$colors[$i]}};"><a href="#publications" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[($i%count($colors))]}};"><a href="#publications" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Publications</span></span></div>
           <!-- end inner --></a></div>
           <? ++$i ?>
@@ -42,7 +44,7 @@
 
       @if(!empty($person->Link4))
 
-        <div style="background-color: {{$colors[$i]}};"><a href="#extras" title="" data-toggle="tab">
+        <div style="background-color: {{$colors[($i%count($colors))]}};"><a href="#extras" title="" data-toggle="tab">
           <div class="inner"><span class="linktext active"><span class="linklabel">Extras</span></span></div>
           <!-- end inner --></a></div>
           <? ++$i ?>
